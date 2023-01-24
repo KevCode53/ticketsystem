e109bb4aa9b84d8d1d1985e75cb61d67e9b4081d
Kevin Palma <kapalma05@gmail.com>
2023-01-23 18:13:17 -0600

    :construction: work in login
    work in login

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,7 +1,9 @@
-import Styles from "@/styles/Login.module.css";
+import styles from "@/styles/Login.module.scss";
 import { login } from "@/utils/login";
+import { useRef } from "react";
 
 const Login = () => {
+  const passRef = useRef();
   const handleSubmit = (e) => {
     e.preventDefault();
     login({
@@ -9,22 +11,45 @@ const Login = () => {
       password: "abc123/-",
     }).then((res) => console.log(res));
   };
+
+  const handleViewPassword = (e) => {
+    if (passRef.current.type === "password") {
+      passRef.current.type = "text";
+    } else {
+      passRef.current.type = "password";
+    }
+  };
   return (
-    <>
-      <div className={Styles.container}>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label>email</label>
-            <input type="text" />
-          </div>
-          <div>
-            <label>password</label>
-            <input type="password" />
-          </div>
-          <button>Login</button>
-        </form>
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <div></div>
+        <div>
+          <header className={styles.header}>
+            <img src="" />
+            <h2>Login</h2>
+            <p>
+              Login in the tickets system for create or manage the tickets
+              report
+            </p>
+          </header>
+          <form onSubmit={handleSubmit}>
+            <div className={styles.input_group}>
+              <input type="text" required />
+              <label>email</label>
+              <button type="button">@</button>
+            </div>
+            <div className={styles.input_group}>
+              <input ref={passRef} type="password" required />
+              <label>password</label>
+              <button type="button" onClick={handleViewPassword}>
+                @
+              </button>{" "}
+            </div>
+            <button className="btn btn-primary">Login</button>
+          </form>
+        </div>
       </div>
-    </>
+    </main>
   );
 };
 
